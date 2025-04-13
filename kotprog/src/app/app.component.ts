efac3998093b27a51d558fdc5ea2e718c416afa2
Csonka Dániel Botond <h152489@stud.u-szeged.hu>
2025-04-13 17:33:10 +0200

    turheto funkcionalitas, undorito kinezet

--- kotprog/src/app/app.component.ts
+++ kotprog/src/app/app.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { PhoneListComponent } from './components/phone-list/phone-list.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatBadgeModule,
     PhoneListComponent
   ],
-  standalone: true
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Phone Shop';
+  title = 'Phone Store';
 }
